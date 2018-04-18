299485f1629933c531015aa02e55c2bb53cc0f02
Samarth Agarwal <samarthagarwal@live.com>
2018-04-18 19:33:16 +0530

    Initial Release

---
+++ README.md
@@ -0,0 +1,35 @@
+# woocommerce_api
+
+A dart package to interact with the WooCommerce API. It uses OAuth1.0a behind the scenes to generate the signature and URL string. It then makes calls and return the data back to the calling function.
+
+## Getting Started
+
+- Import the package
+
+`import 'package:woocommerce_api/woocommerce_api.dart';`
+
+- Initialize the SDK
+
+```
+WooCommerceAPI wc_api = new WooCommerceAPI(
+    "http://www.mywoocommerce.com",
+    "ck_...",
+    "cs_..."
+);
+```
+
+- Use functions
+
+```
+List _products = new List();
+
+wc_api.getAsync("products?page=2").then((val) {  
+    List parsedMap = JSON.decode(val.body);
+    setState(() {
+    parsedMap.forEach((f){
+        _products.add(f);
+    }); 
+    print(_products.length);
+    });
+});
+```
---
+++ lib/woocommerce_api.dart
@@ -0,0 +1,144 @@
+library woocommerce_api;
+
+import 'dart:async';
+import "dart:collection";
+import 'dart:convert';
+import "dart:math";
+import "dart:core";
+import 'package:crypto/crypto.dart' as crypto;
+import 'package:http/http.dart';
+import 'package:query_string/query_string.dart';
+import 'package:http/http.dart' as http;
+
+class WooCommerceAPI {
+  var url;
+  var consumerKey;
+  var consumerSecret;
+
+  WooCommerceAPI(url, consumerKey, consumerSecret){
+    this.url = url;
+    this.consumerKey = consumerKey;
+    this.consumerSecret = consumerSecret;
+  }
+
+
+  _getOAuthURL(String request_method, String endpoint) {
+    var consumerKey = this.consumerKey; //"ck_4e943ec0f3c76eba33fffac4b7fc0d2f1f3ca91a";
+    var consumerSecret = this.consumerSecret; //"cs_fbb723138e354e30c3d4d4e0c0f95389bf610044";
+    var token = "";
+    var token_secret = "";
+    var url = this.url + "/wp-json/wc/v2/" + endpoint;
+    var containsQueryParams = url.contains("?");
+
+    var rand = new Random();
+    var codeUnits = new List.generate(10, (index) {
+      return rand.nextInt(26) + 97;
+    });
+
+    var nonce = new String.fromCharCodes(codeUnits);
+    int timestamp = (new DateTime.now().millisecondsSinceEpoch / 1000).toInt();
+
+    print(timestamp);
+    print(nonce);
+
+    var method = request_method;
+    var path = url.split("?")[0];
+    var parameters = "oauth_consumer_key=" +
+        consumerKey +
+        "&oauth_nonce=" +
+        nonce +
+        "&oauth_signature_method=HMAC-SHA1&oauth_timestamp=" +
+        timestamp.toString() +
+        "&oauth_token=" +
+        token +
+        "&oauth_version=1.0&";
+
+    if (containsQueryParams == true) {
+      parameters = parameters + url.split("?")[1];
+    } else {
+      parameters = parameters.substring(0, parameters.length - 1);
+    }
+
+    Map<dynamic, dynamic> params = QueryString.parse(parameters);
+    Map<dynamic, dynamic> treeMap = new SplayTreeMap<dynamic, dynamic>();
+    treeMap.addAll(params);
+
+    String parameterString = "";
+
+    for (var key in treeMap.keys) {
+      parameterString = parameterString +
+          Uri.encodeQueryComponent(key) +
+          "=" +
+          treeMap[key] +
+          "&";
+    }
+
+    parameterString = parameterString.substring(0, parameterString.length - 1);
+
+    var baseString = method +
+        "&" +
+        Uri.encodeQueryComponent(
+            containsQueryParams == true ? url.split("?")[0] : url) +
+        "&" +
+        Uri.encodeQueryComponent(parameterString);
+
+    print(baseString);
+
+    var signingKey = consumerSecret + "&" + token;
+    print(signingKey);
+    //print(UTF8.encode(signingKey));
+    var hmacSha1 =
+        new crypto.Hmac(crypto.sha1, UTF8.encode(signingKey)); // HMAC-SHA1
+    var signature = hmacSha1.convert(UTF8.encode(baseString));
+
+    print(signature);
+
+    var finalSignature = base64Encode(signature.bytes);
+    print(finalSignature);
+
+    var requestUrl = "";
+
+    if (containsQueryParams == true) {
+      print(url.split("?")[0] +
+          "?" +
+          parameterString +
+          "&oauth_signature=" +
+          Uri.encodeQueryComponent(finalSignature));
+      requestUrl = url.split("?")[0] +
+          "?" +
+          parameterString +
+          "&oauth_signature=" +
+          Uri.encodeQueryComponent(finalSignature);
+    } else {
+      print(url +
+          "?" +
+          parameterString +
+          "&oauth_signature=" +
+          Uri.encodeQueryComponent(finalSignature));
+      requestUrl = url +
+          "?" +
+          parameterString +
+          "&oauth_signature=" +
+          Uri.encodeQueryComponent(finalSignature);
+    }
+
+    return requestUrl;
+  }
+
+  Future<Response> getAsync(String endPoint) async {
+
+    var url = this._getOAuthURL("GET", endPoint);
+
+    return http.get(url);
+     
+  }
+
+  postAsync(String endPoint, Object data) async {
+
+    var url = this._getOAuthURL("POST", endPoint);
+
+    final response = await http.post(url, body: data);
+
+    return JSON.decode(response.body); 
+  }
+}
